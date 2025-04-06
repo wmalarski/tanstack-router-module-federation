8b58e3480001331af93517ddabb93ec34fc0849e
Wojtek Malarski <w.malarski@gmail.com>
2025-04-06 19:36:17 +0200

    moves to biome

--- packages/ui/turbo/generators/config.ts
+++ packages/ui/turbo/generators/config.ts
@@ -5,25 +5,25 @@ import type { PlopTypes } from "@turbo/gen";
 export default function generator(plop: PlopTypes.NodePlopAPI): void {
   // A simple generator to add a new React component to the internal UI library
   plop.setGenerator("react-component", {
-    description: "Adds a new react component",
-    prompts: [
-      {
-        type: "input",
-        name: "name",
-        message: "What is the name of the component?",
-      },
-    ],
     actions: [
       {
-        type: "add",
         path: "src/{{kebabCase name}}.tsx",
         templateFile: "templates/component.hbs",
+        type: "add",
       },
       {
-        type: "append",
         path: "package.json",
         pattern: /"exports": {(?<insertion>)/g,
         template: '    "./{{kebabCase name}}": "./src/{{kebabCase name}}.tsx",',
+        type: "append",
+      },
+    ],
+    description: "Adds a new react component",
+    prompts: [
+      {
+        message: "What is the name of the component?",
+        name: "name",
+        type: "input",
       },
     ],
   });
