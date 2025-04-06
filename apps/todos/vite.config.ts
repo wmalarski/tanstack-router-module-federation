36851408cc1d3d3d34fb4744c002213ac6622ef7
Wojtek Malarski <w.malarski@gmail.com>
2025-04-06 20:33:50 +0200

    components loaded

--- apps/todos/vite.config.ts
+++ apps/todos/vite.config.ts
@@ -1,11 +1,9 @@
 import { federation } from "@module-federation/vite";
 import react from "@vitejs/plugin-react";
-import { writeFileSync } from "node:fs";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { dependencies } from "./package.json";
 
-export default defineConfig(({ mode }) => {
-  const selfEnv = loadEnv(mode, process.cwd());
+export default defineConfig(() => {
   return {
     // server: {
     //   fs: {
@@ -16,16 +14,6 @@ export default defineConfig(({ mode }) => {
       target: "chrome89",
     },
     plugins: [
-      {
-        name: "generate-environment",
-        options: () => {
-          console.info("selfEnv", selfEnv);
-          writeFileSync(
-            "./src/environment.ts",
-            `export default ${JSON.stringify(selfEnv, null, 2)};`,
-          );
-        },
-      },
       federation({
         filename: "remoteEntry.js",
         name: "todos",
